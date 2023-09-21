ec6e255e647d07067322d3904d2147f54b7636c6
Jeff North <jnorthdata@gmail.com>
2023-09-21 11:54:25 -0700

    Demographic Info working

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,32 @@
-// Function to handle the select element change event
 function optionChanged(selectedValue) {
     // When the user selects a new value, update the demographic info and charts
     updateDemographicInfo(selectedValue);
     updateCharts(selectedValue);
   }
 
-// Function to update the demographic information
+
 function updateDemographicInfo(subjectId) {
-    // You can update the demographic info here based on the selected subject ID
-    // For example, you can use fetch() to retrieve data from an API or manipulate
-    // existing data to display it in the #sample-metadata div.
+ 
+
+  // Access the data.metadata array to find the matching metadata object
+  const metadata = data.metadata.find(item => item.id === parseInt(subjectId))
+  console.log("UPDATE FUNCTION");
+
+  // Get a reference to the #sample-metadata div where the info will be displayed
+  const infoBox = document.getElementById("sample-metadata");
+
+  // Clear the previous content of the info box
+  infoBox.innerHTML = "";
+
+  // Loop through the metadata object and display each key-value pair
+  for (const [key, value] of Object.entries(metadata)) {
+    const infoItem = document.createElement("p");
+    infoItem.innerHTML = `<strong>${key}:</strong> ${value}`;
+    infoBox.appendChild(infoItem);
   }
+}
+
+   
   
   // Function to update the charts
   function updateCharts(subjectId) {
@@ -24,25 +40,29 @@ function updateDemographicInfo(subjectId) {
 // Define the URL for fetching data
 const dataURL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
     
+// Define a global variable for data
+let data;
+
 
 document.addEventListener('DOMContentLoaded', function() {
   
     // Fetch data from the URL using D3.js
-    d3.json(dataURL).then(data => {
-        const dropdown = document.getElementById("selDataset");
-        data.names.forEach(name => {
-        const option = document.createElement("option");
-        option.value = name;
-        option.text = name;
-        dropdown.appendChild(option);
+    d3.json(dataURL).then(dataResponse => {
+      data = dataResponse
+      const dropdown = document.getElementById("selDataset");
+      data.names.forEach(name => {
+      const option = document.createElement("option");
+      option.value = name;
+      option.text = name;
+      dropdown.appendChild(option);
         });
   
   
     // Call optionChanged with the default value
     optionChanged(data.names[0]);
-  }).catch(error => {
-    console.error("Error fetching data:", error);
-  });
+    }).catch(error => {
+      console.error("Error fetching data:", error);
+    });
   
    
   });
